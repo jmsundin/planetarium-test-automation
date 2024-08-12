0ef9de480871afbcd08301f4ad49e7d1bef3b7c2
katrine605 <katrine605@revature.net>
2024-08-12 15:10:01 -0400

    adding Then for moon addition

--- src/test/java/com/revature/step/PlanetAdditionSteps.java
+++ src/test/java/com/revature/step/PlanetAdditionSteps.java
@@ -3,6 +3,8 @@ package com.revature.step;
 import com.revature.Setup;
 import org.junit.Assert;
 import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import com.revature.TestRunner;
@@ -49,6 +51,13 @@ public class PlanetAdditionSteps {
         String alert = TestRunner.driver.switchTo().alert().getText();
         TestRunner.driver.switchTo().alert().accept();
         Assert.assertTrue(alert.contains("please try again"));
+    }
+
+    @Then("The planet {string} should be added to the Planetarium")
+    public void the_planet_should_be_added_to_the_Planetarium(String planetName) {
+        String xpath = "//tr[td[1][text()='planet'] and td[3][text()='%s']]".formatted(planetName);
+        WebElement planet = TestRunner.wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+        Assert.assertTrue(planet.isDisplayed());;
     }
 
 }
