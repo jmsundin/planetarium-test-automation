cd61b7b2897892f800ff1795d70c723869e98586
katrine605 <katrine605@revature.net>
2024-08-12 16:14:41 -0400

    adding error handling for planet deletion steps

--- src/test/java/com/revature/step/PlanetDeletionSteps.java
+++ src/test/java/com/revature/step/PlanetDeletionSteps.java
@@ -35,10 +35,10 @@ public class PlanetDeletionSteps {
     @Then("The planet with the id {string} should be deleted from the Planetarium")
     public void the_planet_with_the_id_should_be_deleted_from_the_Planetarium(String string) {
         String xpath = "//tr[td[1][text()='planet'] and td[2][text()='" + string + "']]";
-        WebElement element = TestRunner.driver.findElement(By.xpath(xpath));
+        // WebElement element = TestRunner.driver.findElement(By.xpath(xpath));
 
         try{
-            TestRunner.wait.until(ExpectedConditions.invisibilityOf(element));
+            TestRunner.wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(xpath)));
         } catch (TimeoutException e) {
             Assert.fail(e.getMessage());
         }
@@ -79,9 +79,10 @@ public class PlanetDeletionSteps {
     @Then("The user should no longer see {string} and {string} within the planetarium")
     public void the_user_should_no_longer_see_and_within_the_planetarium(String string, String string2) {
         try {
+            TestRunner.wait.until(ExpectedConditions.alertIsPresent());
             TestRunner.driver.switchTo().alert().accept();
             Assert.fail();
-        } catch (NoAlertPresentException ignored){
+        } catch (NoAlertPresentException | TimeoutException ignored){
 
         }
 
@@ -89,12 +90,9 @@ public class PlanetDeletionSteps {
         String moonXpath = "//tr[td[1][text()='moon'] and td[3][text()='" + string2 + "']]";
 
         try{
-            WebElement planetElement = TestRunner.driver.findElement(By.xpath(planetXpath));
-            WebElement moonElement = TestRunner.driver.findElement(By.xpath(moonXpath));
 
-            TestRunner.wait.until(ExpectedConditions.invisibilityOf(planetElement));
-            TestRunner.wait.until(ExpectedConditions.invisibilityOf(moonElement));
-
+            TestRunner.wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(planetXpath)));
+            TestRunner.wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(moonXpath)));
 
         } catch (TimeoutException e){
             Assert.fail(e.getMessage());
