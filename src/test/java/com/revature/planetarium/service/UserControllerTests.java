752e84a21a58b5cdb971afea8f116ea54651858b
Jon Sundin <sundinjon@gmail.com>
2024-08-15 20:55:41 -0700

    passwordTooLongTest

--- src/test/java/com/revature/planetarium/service/UserControllerTests.java
+++ src/test/java/com/revature/planetarium/service/UserControllerTests.java
@@ -90,7 +90,10 @@ public class UserControllerTests {
 
     @Test
     public void passwordTooLongTest(){
-        // TODO
+        int id = 5;
+        User newUser = new User(id, validUsername1, "a".repeat(31));
+        Exception exception = assertThrows(UserFail.class, () -> userService.createUser(newUser));
+        assertEquals("Password must be between 1 and 30 characters", exception.getMessage());
     }
 
     @Test
