acd6f958cb602350b57763e92c1ea0a06b800191
Jon Sundin <sundinjon@gmail.com>
2024-08-26 13:47:32 -0700

    test implementations

--- src/test/java/com/revature/planetarium/repository/planet/PlanetDaoTest.java
+++ src/test/java/com/revature/planetarium/repository/planet/PlanetDaoTest.java
@@ -52,5 +52,120 @@ public class PlanetDaoTest {
         existingPlanet.setPlanetName("Earth");
         existingPlanet.setOwnerId(1);
     }
-    
+
+    @Test
+    public void testCreatePlanetWithImagePositive(){
+        validPlanet.setImageData(planetImage);
+        Optional<Planet> newPlanet = planetDao.createPlanet(validPlanet);
+        Assert.assertNotNull(newPlanet);
+        Assert.assertEquals("Triton", newPlanet.get().getPlanetName());
+    }
+
+    @Test
+    public void testCreatePlanetNoImagePositive(){
+        Optional<Planet> newPlanet = planetDao.createPlanet(validPlanet);
+        Assert.assertNotNull(newPlanet);
+        Assert.assertEquals("Triton", newPlanet.get().getPlanetName());
+    }
+
+    @Test
+    public void testCreatePlanetNegative(){
+        Planet planet = new Planet();
+        Assert.assertThrows(PlanetFail.class, ()->{
+            planetDao.createPlanet(planet);
+        });
+    }
+
+    @Test
+    public void testReadPlanetIDPositive(){
+        Optional<Planet> checkPlanet = planetDao.readPlanet(1);
+        Assert.assertNotNull(checkPlanet);
+        Assert.assertEquals(existingPlanet, checkPlanet.get());
+    }
+
+    @Test
+    public void testReadPlanetIDNegative(){
+        Optional<Planet> notExistingPlanet = planetDao.readPlanet(100);
+        Assert.assertFalse(notExistingPlanet.isPresent());
+    }
+
+    @Test
+    public void testReadPlanetNamePositive(){
+        Optional<Planet> checkPlanet = planetDao.readPlanet("Earth");
+        Assert.assertNotNull(checkPlanet);
+        Assert.assertEquals(existingPlanet, checkPlanet.get());
+
+    }
+
+    @Test
+    public void testReadPlanetNameNegative(){
+        Optional<Planet> notExistingPlanet = planetDao.readPlanet("Not a Planet");
+        Assert.assertFalse(notExistingPlanet.isPresent());
+    }
+
+     @Test
+    public void testGetAllPlanetsPositive(){
+        Assert.assertTrue(planetDao.readAllPlanets().size() >= 4);
+    }
+
+    @Test
+    public void testGetAllPlanetsNegative(){
+        deletePlanetsForNegativeGetAllPlanetsTest();
+        Assert.assertEquals(0, planetDao.readAllPlanets().size());
+    }
+
+    // helper method for negative getAllPlanets test
+    public void deletePlanetsForNegativeGetAllPlanetsTest() {
+        try(Connection connection = DatabaseConnector.getConnection()){
+            String sql = "delete from planets";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+        } catch (SQLException e){
+            throw new AssertionError("Could not delete planets");
+        }
+    }
+
+    @Test
+    public void testUpdatePlanetNamePositive(){
+        existingPlanet.setPlanetName("Mars");
+        Assert.assertSame(existingPlanet, planetDao.updatePlanet(existingPlanet).get());
+    }
+
+    @Test
+    public void testUpdatePlanetPlanetPositive(){
+        existingPlanet.setOwnerId(2);
+        Assert.assertSame(existingPlanet, planetDao.updatePlanet(existingPlanet).get());
+    }
+
+    @Test
+    public void testUpdatePlanetNegative(){
+        Planet planet = new Planet();
+        Assert.assertSame(Optional.empty(),planetDao.updatePlanet(planet));
+    }
+
+    @Test
+    public void testDeletePlanetByIDPositive(){
+        Assert.assertTrue(planetDao.deletePlanet(1));
+        Assert.assertEquals(3, planetDao.readAllPlanets().size());
+        Assert.assertSame(Optional.empty(),planetDao.readPlanet(1));
+    }
+
+    @Test
+    public void testDeletePlanetByIDNegative(){
+        Assert.assertFalse(planetDao.deletePlanet(100));
+        Assert.assertEquals(4, planetDao.readAllPlanets().size());
+    }
+
+    @Test
+    public void testDeletePlanetByNamePositive(){
+        Assert.assertTrue(planetDao.deletePlanet("Earth"));
+        Assert.assertEquals(3, planetDao.readAllPlanets().size());
+        Assert.assertSame(Optional.empty(),planetDao.readPlanet(1));
+    }
+
+    @Test
+    public void testDeletePlanetByNameNegative(){
+        Assert.assertFalse(planetDao.deletePlanet("Not a Planet"));
+        Assert.assertEquals(4, planetDao.readAllPlanets().size());
+    }
 }
