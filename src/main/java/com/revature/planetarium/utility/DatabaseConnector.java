0d0d76d8af39f6de5a30da3b527c06d7226896c6
RileyJatRevature <riley593@revature.com>
2024-08-29 11:58:54 -0400

    Updated POM and set Database connector to connect to AWS RDS

--- src/main/java/com/revature/planetarium/utility/DatabaseConnector.java
+++ src/main/java/com/revature/planetarium/utility/DatabaseConnector.java
@@ -3,16 +3,28 @@ package com.revature.planetarium.utility;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.sqlite.SQLiteConfig;
 
 public class DatabaseConnector {
 
-    public static Connection getConnection() throws SQLException {
+    /*public static Connection getConnection() throws SQLException {
         SQLiteConfig config = new SQLiteConfig();
         config.enforceForeignKeys(true);
         String url = System.getenv("PLANETARIUM");
         return DriverManager.getConnection(url, config.toProperties());
-    }
+    }*/
 
+    public static Connection getConnection() throws SQLException{
+        Properties properties = new Properties();
+
+        //todo replace hard values with system environment variables
+        properties.setProperty("user", System.getenv("PLANETARIUM_USERNAME"));
+        properties.setProperty("password", System.getenv("PLANETARIUM_PASSWORD"));
+
+        String url = System.getenv("PLANETARIUM_URL");
+
+        return DriverManager.getConnection(url, properties);
+    }
 }
